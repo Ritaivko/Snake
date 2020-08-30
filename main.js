712b385412429792e035ade72ae539991013a6a3
Ritaivko <rita26111@gmail.com>
2020-08-30 21:19:22 +0300

    initial commit

--- main.js
+++ main.js
@@ -20,25 +20,56 @@
 let snake = document.getElementById('snake');
 
 const windowWidth = document.documentElement.clientWidth;
-
+const windowHeight = document.documentElement.clientHeight;
 const snakeWidth = 384;
+const left = '37';
+const up = '38';
+const right = '39';
+const down = '40';
 
 createInterval(snake);
 
+document.addEventListener('keydown', rotateSnake);
+
 function createInterval(element) {
     let offset = -snakeWidth;
-    return setInterval(() => {
+    let isAdded = false;
+    let interval = setInterval(() => {
         element.style.left = offset + 'px';
         offset += 5;
-        if (windowWidth === snakeWidth + offset) {
+        if (windowWidth <= snakeWidth + offset && !isAdded) {
+            isAdded = true;
             let newSnake = document.createElement('div');
-            newSnake.innerHTML = snake.innerHTML;
+            newSnake.innerHTML = element.innerHTML;
             newSnake.classList.add(['snake']);
-            newSnake.style.left = -snakeWidth;
+            newSnake.style.left = -snakeWidth + 'px';
             document.body.appendChild(newSnake);
             createInterval(newSnake);
+        }
+        if (offset >= windowWidth) {
+            element.remove();
+            clearInterval(interval);
         }
     }, 100)
 }
 
-
+function rotateSnake(event) {
+    switch (event.keyCode) {
+        case left: {
+            console.log('left');
+            break;
+        } 
+        case up: {
+            console.log('up');
+            break;
+        }
+        case right: {
+            console.log('right');
+            break;
+        }
+        case down: {
+            console.log('down');
+            break;
+        }
+    }
+}
